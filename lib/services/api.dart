3c14b3640cba39b82fc39504cc3ee91d4ac0ef81
Daniil_Shurygin <shurygindv@ya.ru>
2020-08-21 23:33:37 +0400

    api: add converter api / mapping

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -5,8 +5,8 @@ import 'dart:convert';
 const rateurl =
     'https://ia500r2mmf.execute-api.eu-west-3.amazonaws.com/default/get-currency-rating';
 
-const converterUrl =
-    'https://nzun06d9r4.execute-api.eu-west-3.amazonaws.com/default/convertCurrency';
+const converterOrigin = 'nzun06d9r4.execute-api.eu-west-3';
+const converterPath = '/default/convertCurrency';
 
 enum CurrencyType { USD, RUB, EUR }
 
@@ -28,12 +28,12 @@ class Currency {
   }
 }
 
-class CurrencyInfo {
+class CurrencyRateResult {
   final Currency usd;
   final Currency eur;
   final String updateTime;
 
-  CurrencyInfo({this.usd, this.eur, this.updateTime});
+  CurrencyRateResult({this.usd, this.eur, this.updateTime});
 
   double getUSDRateIn(String key) {
     return usd.getRate(key);
@@ -43,31 +43,62 @@ class CurrencyInfo {
     return eur.getRate(key);
   }
 
-  factory CurrencyInfo.fromJson(String updateTime, Map<String, dynamic> data) =>
-      CurrencyInfo(
+  factory CurrencyRateResult.fromJson(
+          String updateTime, Map<String, dynamic> data) =>
+      CurrencyRateResult(
           usd: Currency.fromJson(data['USD'] as Map<String, dynamic>),
           eur: Currency.fromJson(data['EUR'] as Map<String, dynamic>),
           updateTime: updateTime);
 }
 
-CurrencyInfo mapCurrencyRating(String body) {
+CurrencyRateResult mapCurrencyRating(String body) {
   final parsed = json.decode(body);
 
   final data = parsed['data'] as Map<String, dynamic>;
   final updateTime = parsed['date'] as String;
 
-  return CurrencyInfo.fromJson(updateTime, data);
+  return CurrencyRateResult.fromJson(updateTime, data);
+}
+
+class ConverterResult {
+  final double converted;
+
+  ConverterResult({this.converted});
+
+  factory ConverterResult.fromJson(Map<String, dynamic> data) =>
+      ConverterResult(converted: data['converted']);
+}
+
+ConverterResult mapConverterValues(String body) {
+  final parsed = json.decode(body);
+
+  final data = parsed['data'] as Map<String, dynamic>;
+
+  return ConverterResult.fromJson(data);
+}
+
+Uri getConverterUri(CurrencyType from, CurrencyType to) {
+  final queryParameters = {
+    'from': from.toString(),
+    'to': to.toString(),
+  };
+
+  print(queryParameters);
+
+  return Uri.https(converterOrigin, converterPath, queryParameters);
 }
 
 class CurrencyApi {
-  static Future<CurrencyInfo> fetchRate() async {
+  static Future<CurrencyRateResult> fetchRate() async {
     final res = await http.get(rateurl);
 
     return compute(mapCurrencyRating, res.body);
   }
 
-  static Future<http.Response> convertRate(String currency,
-      {CurrencyType from, CurrencyType to}) {
-    return http.get(converterUrl);
+  static Future<ConverterResult> convertRate(String currency,
+      {CurrencyType from, CurrencyType to}) async {
+    final res = await http.get(getConverterUri(from, to));
+
+    return compute(mapConverterValues, res.body);
   }
 }
